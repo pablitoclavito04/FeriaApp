a8354ed610120d694ed86791ece35851aa832695
pablitoclavito04 <sanzaznarp@gmail.com>
2026-04-08 09:11:17 +0200

    Se ha actualizado el archivo server.js

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,17 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 const authRoutes = require('./src/routes/authRoutes');
+const feriaRoutes = require('./src/routes/feriaRoutes');
+const casetaRoutes = require('./src/routes/casetaRoutes');
+const menuRoutes = require('./src/routes/menuRoutes');
+const conciertoRoutes = require('./src/routes/conciertoRoutes');
 
-// Configuración de variables de entorno
 dotenv.config();
 
-// Conectar a MongoDB
 connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
@@ -22,13 +23,16 @@ app.use(cors());
 
 // Rutas
 app.use('/api/auth', authRoutes);
+app.use('/api/ferias', feriaRoutes);
+app.use('/api/casetas', casetaRoutes);
+app.use('/api/menus', menuRoutes);
+app.use('/api/conciertos', conciertoRoutes);
 
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.json({ message: 'FeriaApp API funcionando correctamente' });
 });
 
-// Puerto
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
