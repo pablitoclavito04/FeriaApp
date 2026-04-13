5c6fb3a8f92e5223c4c121d34fe761e7a7b45e31
pablitoclavito04 <sanzaznarp@gmail.com>
2026-04-13 23:22:38 +0200

    The official fair map has been added to the MapPicker component

--- frontend/src/components/MapPicker.jsx
+++ frontend/src/components/MapPicker.jsx
@@ -1,6 +1,7 @@
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import { MapContainer, ImageOverlay, Marker, useMapEvents } from 'react-leaflet';
 import { useState } from 'react';
 import L from 'leaflet';
+import planoFeria from '../assets/plano_feria.png';
 
 // Fix default marker icon
 delete L.Icon.Default.prototype._getIconUrl;
@@ -9,6 +10,9 @@ L.Icon.Default.mergeOptions({
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
+
+// Bounds del plano (ajusta estos valores según tu imagen)
+const bounds = [[0, 0], [1052, 1514]];
 
 const LocationPicker = ({ onLocationSelect }) => {
   useMapEvents({
@@ -31,14 +35,19 @@ const MapPicker = ({ onLocationSelect, initialPosition }) => {
     <div className="map-picker">
       <p className="map-instructions">Click on the map to select the caseta location</p>
       <MapContainer
-        center={[36.6850, -6.1261]}
-        zoom={15}
-        style={{ height: '400px', width: '100%', borderRadius: '8px' }}
+        center={[526, 757]}
+        zoom={-1}
+        crs={L.CRS.Simple}
+        style={{ height: '500px', width: '100%', borderRadius: '8px' }}
+        minZoom={-2}
+        maxZoom={3}
+        maxBounds={bounds}
+        maxBoundsViscosity={1.0}
+        whenReady={(map) => {
+          map.target.fitBounds(bounds);
+        }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; OpenStreetMap contributors'
-        />
+        <ImageOverlay url={planoFeria} bounds={bounds} />
         <LocationPicker onLocationSelect={handleLocationSelect} />
         {marker && <Marker position={marker} />}
       </MapContainer>
